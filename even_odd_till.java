2070818a805b4dd7d41a6d1160fe4fc91a447958
git-ankitgautam <ankitgautam485@gmail.com>
2022-03-09 21:58:11 +0530

    09-03-22 22:00

--- even_odd_till.java
+++ even_odd_till.java
@@ -3,48 +3,36 @@ public class even_odd_till
 {
     public static void main(String[] args)
     {
+        int index = 1;
         System.out.println("Enter the number you want to go till:-");
         Scanner in = new Scanner(System.in);
         int lim = in.nextInt();
-        int[] even = even(lim);
-        int[] odd = odd(lim);
-        System.out.println("There are " + even[0] + " Even numbers till "+ lim + " :-");
-        printarr(even,even[0]);
-        System.out.println("\nThere are " + odd[0] + " Odd numbers till " + lim + ":-");
-        printarr(odd,odd[0]);
-    }   
-
-    public static int[] even(int x)
-    {
-        int index=1;
-        int[] arr = new int[x];
-        for(int i=1;i<=x;i++)
+        int[] even = new int[(lim/2)+1];
+        for(int i=1;i<=lim;i++)
         {
             if(i %2 == 0)
             {
-                arr[index] = i;
+                even[index] = i;
                 index++;
             }
         }
-        arr[0] = index;
-        return arr;
-    }
-
-    public static int[] odd(int x)
-    {
-        int index=1;
-        int[] arr = new int[x];
-        for(int i=1;i<=x;i++)
+        even[0] = index;
+        System.out.println("There are " + even[0] + " Even numbers till "+ lim + " :-");
+        printarr(even,even[0]);
+        index = 1;
+        int[] odd = new int[(lim/2)+1];
+        for(int i=1;i<=lim;i++)
         {
             if(i %2 != 0)
             {
-                arr[index] = i;
+                odd[index] = i;
                 index++;
             }
         }
-        arr[0] =index;
-        return arr;
-    }
+        odd[0] = index;
+        System.out.println("\nThere are " + odd[0] + " Odd numbers till " + lim + ":-");
+        printarr(odd,odd[0]);
+    }   
 
     public static void printarr(int[] arr,int size)
     {
