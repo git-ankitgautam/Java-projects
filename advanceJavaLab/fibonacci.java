fddb206d7ae50e6817cfca74332228ffd7c588de
git-ankitgautam <ankitgautam485@gmail.com>
2023-02-06 23:25:09 +0530

    06-02-23 23.30

--- advanceJavaLab/fibonacci.java
+++ advanceJavaLab/fibonacci.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class fibonacci 
 {
-    public int fibonacci(int t)
+    public static int fibonacci(int t)
     {
         if ( t == 0 )
             return 0;
@@ -12,17 +12,17 @@ public class fibonacci
             return ( fibonacci(t-1) + fibonacci(t-2));
     }
 
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         int i=0,num;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of terms:-");
         num = input.nextInt();
         input.close();
-        System.out.println("Fibonacci series:-");
+        System.out.print("Fibonacci series:-");
         for (i=0;i<num;i++)
         {  
-            System.out.println(fibonacci(i));
+            System.out.print(fibonacci(i) + " ");
         }
         
     }  
--- advanceJavaLab/tempCodeRunnerFile.java
+++ advanceJavaLab/tempCodeRunnerFile.java
@@ -1,1 +1,26 @@
- tf.setBounds(50,50,150,20); 
+import java.util.Scanner;
+
+public class checkNumIsPalindrome {
+    
+    public static void main(String args[])
+    {
+        int temp, num, rem,sum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number:-");
+        num = input.nextInt();
+        input.close();
+        temp = num;
+        while(num>0)
+        {    
+            rem=num%10;  
+            sum=(sum*10)+rem;    
+            num/=10;
+        }
+
+        if(temp == sum)    
+            System.out.println(temp + " is a palindrome number ");    
+        else    
+            System.out.println(temp + "is not palindrome");     
+
+    }
+}
